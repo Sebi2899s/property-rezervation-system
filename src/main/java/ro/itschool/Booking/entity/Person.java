dcf155009bc278ff557e89f2f749126c7d97b734
Szekely Sebastian <szekely.sebastian@gloobus.it>
2023-06-27 15:31:42 +0300

    -implementing Builder Design Pattern for Person and Property classes

--- src/main/java/ro/itschool/Booking/entity/Person.java
+++ src/main/java/ro/itschool/Booking/entity/Person.java
@@ -3,11 +3,13 @@ package ro.itschool.Booking.entity;
 import com.fasterxml.jackson.annotation.JsonBackReference;
 import jakarta.persistence.*;
 import lombok.*;
+import org.springframework.cglib.core.Local;
 import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.security.core.userdetails.UserDetails;
 import ro.itschool.Booking.DtoEntity.PersonDTO;
 
+import java.time.LocalDate;
 import java.util.Collection;
 import java.util.List;
 import java.util.Objects;
@@ -15,7 +17,7 @@ import java.util.Objects;
 @Entity
 @Getter
 @Setter
-@Builder
+//@Builder
 @AllArgsConstructor
 @NoArgsConstructor
 public class Person implements UserDetails {
@@ -32,9 +34,9 @@ public class Person implements UserDetails {
     private String password;
 
 
-    private String checkIn;
+    private LocalDate checkIn;
 
-    private String checkOut;
+    private LocalDate checkOut;
 
     @Enumerated(EnumType.STRING)
     private Role role;
@@ -100,5 +102,63 @@ public class Person implements UserDetails {
     @Override
     public boolean isEnabled() {
         return true;
+    }
+
+    public static PersonBuilder builder(){
+        return new PersonBuilder();
+    }
+
+    //implementing Builder Design Pattern for Person class
+    public static class PersonBuilder{
+        private Person person;
+
+        private PersonBuilder(){
+            this.person=new Person();
+        }
+
+        private PersonBuilder personId(Long id){
+            this.person.personId=id;
+            return this;
+        }
+        public PersonBuilder firstName(String firstName) {
+            this.person.firstName = firstName;
+            return this;
+        }
+        public PersonBuilder lastName(String lastName) {
+            this.person.lastName = lastName;
+            return this;
+        }
+        public PersonBuilder email(String email) {
+            this.person.email = email;
+            return this;
+        }
+        public PersonBuilder password(String password) {
+            this.person.password = password;
+            return this;
+        }
+        public PersonBuilder checkIn(LocalDate checkIn){
+            this.person.checkIn=checkIn;
+            return this;
+        }
+        public PersonBuilder checkOut(LocalDate checkOut){
+            this.person.checkOut=checkOut;
+            return this;
+        }
+        public PersonBuilder role(Role role){
+            this.person.role=role;
+            return this;
+        }
+
+        public PersonBuilder mobileNumber(String mobileNumber) {
+            this.person.mobileNumber = mobileNumber;
+            return this;
+        }
+        public PersonBuilder property(Property property) {
+            this.person.property = property;
+            return this;
+        }
+        public Person build(){
+            return this.person;
+        }
     }
 }
--- src/main/java/ro/itschool/Booking/entity/Property.java
+++ src/main/java/ro/itschool/Booking/entity/Property.java
@@ -13,7 +13,7 @@ import java.util.List;
 @ToString
 @AllArgsConstructor
 @NoArgsConstructor
-@Builder
+//@Builder
 public class Property {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
@@ -35,7 +35,7 @@ public class Property {
     @ToString.Exclude
     private List<Person> personList;
 
-    public Property(Long id, String propertyType, String propertyName, String propertyEmail,  String propertyLocation, String propertyAddress) {
+    public Property(Long id, String propertyType, String propertyName, String propertyEmail, String propertyLocation, String propertyAddress) {
         this.id = id;
         this.propertyType = propertyType;
         this.propertyName = propertyName;
@@ -60,5 +60,44 @@ public class Property {
                 ", propertyAddress='" + propertyAddress + '\'' +
                 ", personList=" + personList +
                 '}';
+    }
+    public static PropertyBuilder builder(){
+        return new PropertyBuilder();
+    }
+//implementing Builder Design Pattern for Property class
+    public static class PropertyBuilder {
+        private Property property;
+
+        private PropertyBuilder() {
+            this.property = new Property();
+        }
+        public PropertyBuilder id(Long id){
+            this.property.id=id;
+            return this;
+        }
+        public PropertyBuilder propertyType(String propertyType){
+            this.property.propertyType=propertyType;
+            return this;
+        }
+        public PropertyBuilder propertyName(String propertyName){
+            this.property.propertyName=propertyName;
+            return this;
+        }
+        public PropertyBuilder propertyEmail(String propertyEmail){
+            this.property.propertyEmail=propertyEmail;
+            return this;
+        }
+        public PropertyBuilder propertyLocation(String propertyLocation){
+            this.property.propertyLocation=propertyLocation;
+            return this;
+        }
+        public PropertyBuilder propertyAddress(String propertyAddress){
+            this.property.propertyAddress=propertyAddress;
+            return this;
+        }
+        public Property build(){
+            return this.property;
+        }
+
     }
 }
